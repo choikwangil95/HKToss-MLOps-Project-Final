9961e3eadddfe52ba7fc2b82dc8d1fda9ec544eb
Yeeun Han <yeaaaun@gmail.com>
2025-05-30 19:43:05 +0900

    0530 NER 코드만 커밋

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from routers import news, status, stock, ner  # 둘 다 임포트
+from routers import news, status, stock, ner  
 
 app = FastAPI(title="MLOps API Server", version="0.0.0")
 
-# 라우터
-app.include_router(status.router)  # 상태 확인 및 헬스 체크 라우터
-app.include_router(news.router)    # 뉴스 관련 라우터
-app.include_router(stock.router)   # 주식 관련 라우터
-app.include_router(ner.router)     # NER 관련 라우터
+# 라우터 등록
+app.include_router(status.router)  # 상태 확인 및 헬스 체크
+app.include_router(news.router)    # 뉴스 관련
+app.include_router(stock.router)   # 주식 관련
+app.include_router(ner.router)     # NER 관련
