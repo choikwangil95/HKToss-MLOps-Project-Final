d1ee7247e20d56a66e80a3e75fce55a257ed7a71
choikwangil95 <choikwangil@ajou.ac.kr>
2025-06-11 10:15:24 +0900

    ner 코드 제거

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from routers import news, status, stock, ner
+from routers import news, status
 
 app = FastAPI(title="MLOps API Server", version="0.0.0")
 
 # 라우터 등록
 app.include_router(status.router)  # 상태 확인 및 헬스 체크
-app.include_router(news.router)    # 뉴스 관련
-app.include_router(stock.router)   # 주식 관련
-app.include_router(ner.router)     # NER 관련
+app.include_router(news.router)  # 뉴스 관련
